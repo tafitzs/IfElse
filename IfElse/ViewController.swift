d2cf41162afb32fb9f43c60ae17216a165844333
tafitzs <tafitzs@hawaii.edu>
2018-01-29 11:53:06 -1000

    Completed If Else

--- IfElse/ViewController.swift
+++ IfElse/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var stringOne = "Yes"
+    var stringTwo = "No"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Create IB Action
+    @IBAction func changColorButtonPressed(_ sender: Any)
+    {
+        //have conditional evaluate to false
+        if stringOne == "No"
+        {
+            //if true, change background color to red
+            self.view.backgroundColor = UIColor.red
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.blue
+            
+        }
+    }
 }
 
