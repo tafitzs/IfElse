6ff354a3469a60f0fa4c5b1cc32686b403c6492e
tafitzs <tafitzs@hawaii.edu>
2018-01-29 11:42:36 -1000

    Initial Commit

---
+++ IfElse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IfElse
+//
+//  Created by Tayler Anne Fitzsimmons on 1/29/18.
+//  Copyright © 2018 Tayler Anne Fitzsimmons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
